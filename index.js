5cc355bae30d07d1633f57bcd07645fcf8ea8ab8
mhcelso <furuyamic@gmail.com>
2019-06-09 17:12:20 -0500

    modelos agregados

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 // exportar funcion que recibe configuracion
+// cuando sea resuelta me devolvera un agente y una metrica
 module.exports = async function (config) {
   const Agent = {}
   const Metric = {}
