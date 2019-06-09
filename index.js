a37ae4523f0398ec0823000928ace6083a3b2daa
mhcelso <furuyamic@gmail.com>
2019-06-09 15:37:04 -0500

    primer modulo

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// exportar funcion que recibe configuracion
+module.exports = async function (config) {
+  const Agent = {}
+  const Metric = {}
+  return {
+    Agent,
+    Metric
+  }
+}
