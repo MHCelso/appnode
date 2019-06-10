1abf72f5eaa7275060db6f2f8d5ecbc3f127aafd
mhcelso <furuyamic@gmail.com>
2019-06-09 23:35:47 -0500

    index modificado uso de modulos

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 'use strict'
 
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
+
 // exportar funcion que recibe configuracion
 // cuando sea resuelta me devolvera un agente y una metrica
 module.exports = async function (config) {
+  const sequelize = setupDatabase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+  AgentModel.hasMany(MetricModel)
+  MetricModel.begingsTo(AgentModel)
+  // realiza promesa conectar a la BD
+  await sequelize.authenticate()
+  // sequelize.sync();
   const Agent = {}
   const Metric = {}
   return {
