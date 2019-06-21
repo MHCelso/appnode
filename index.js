f0efa2d1ac124b500f1a5c72558921df86197acb
mhcelso <furuyamic@gmail.com>
2019-06-20 21:53:55 -0500

    pruebas tests sep

--- index.js
+++ index.js
@@ -3,10 +3,23 @@
 const setupDatabase = require('./lib/db')
 const setupAgentModel = require('./models/agent')
 const setupMetricModel = require('./models/metric')
+const defaults = require('defaults')
 
 // exportar funcion que recibe configuracion
 // cuando sea resuelta me devolvera un agente y una metrica
 module.exports = async function (config) {
+  config = defaults(config, {
+    dialect: 'sqlite',
+    pool: {
+      max: 10,
+      min: 0,
+      idle: 10000
+    },
+    query: {
+      raw: true
+    }
+  })
+
   const sequelize = setupDatabase(config)
   const AgentModel = setupAgentModel(config)
   const MetricModel = setupMetricModel(config)
--- setup.js
+++ setup.js
@@ -2,8 +2,23 @@
 
 const db = require('./')
 const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const prompt = inquirer.createPromptModule()
 
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'esto va a destruir la base de datos, ¿estas seguro?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('No pasa nada')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
@@ -16,13 +31,13 @@ async function setup () {
 
   await db(config).catch(handleFatalError)
 
-  console.log('success')
+  console.log('Success')
   process.exit(0)
 }
 
 function handleFatalError (error) {
-  console.log(error.message)
-  console.log(error.stack)
+  console.error(`${chalk.red('[fatal error]')} ${error.message}`)
+  console.error(error.stack)
   process.exit(1)
 }
 
