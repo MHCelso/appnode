8326719d54a4574c1e619b4220480017105cdcd7
mhcelso <furuyamic@gmail.com>
2019-06-25 21:44:59 -0500

    tests con sinon

--- tests/agent-tests.js
+++ tests/agent-tests.js
@@ -1,17 +1,47 @@
 'use strict'
 
 const test = require('ava')
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
 let db = null
 
 let config = {
   loggin: function () {}
 }
 
+let MetricStub = {
+  belongsTo: sinon.spy()
+} 
+
+let AgentStub = null;
+let sandbox = null
+
+test.afterEach ( () => {
+  sandbox && sandbox.restore()
+})
+
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  sandbox = sinon.createSandbox()
+
+  AgentStub = {
+    hasMany: sandbox.spy() 
+  }
+
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+  })
   db = await setupDatabase(config)
 })
 
+
 test('Agent', t => {
   t.truthy(db.Agent, 'Agent service should exit')
+})
+
+test.serial('Setup', t => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be te Model')
 })
